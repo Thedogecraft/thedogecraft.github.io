d5211a986bda263085c2cca84c802c8a9f675ec7
Thedogecraft <dantaealt@gmail.com>
2023-03-02 16:18:21 -0700

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,14 +6,14 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "dantae",
+  firstName: "Dantae",
   middleName: "",
   lastName: "Romero",
   message: " Passionate about changing the world with technology. ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      url: "https://github.com/thedogecraft",
     },
     
     {
